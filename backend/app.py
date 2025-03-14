e559bcf45d77022942f201e01c5bc99fd5a06b23
ola-je <kidanejerry523@gmail.com>
2025-03-14 11:38:31 +0300

    register inventory analytics blueprint in Flask app

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 from routes.inventory_routes import inventory_routes
 from routes.exchange_routes import exchange_bp
+from routes.inventory_analytics import analytics_bp
 
 
 app = Flask(__name__)
 app.register_blueprint(inventory_routes)
 app.register_blueprint(exchange_bp)
+app.register_blueprint(analytics_bp)
+
 
 
 @app.route('/')
